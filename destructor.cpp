ad876968e0d00d9c445a66f3d1f19bd870cc8e1d
keisyaamalia <kkaysaamalia@gmail.com>
2026-05-04 13:17:53 +0700

    menjalankan program utama

--- destructor.cpp
+++ destructor.cpp
@@ -45,3 +45,12 @@ void angka::isiData()
     }
     cout << endl;
 }
+
+int main()
+{
+    angka belajarcpp(3); //Constructor dipanggil
+    angka *ptrBelajarcpp = new angka(5); //Constructor dipanggil
+    delete ptrBelajarcpp; //Destructor dipanggil
+
+    return 0;
+}

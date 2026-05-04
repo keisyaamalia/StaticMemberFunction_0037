74eb4059a33a4f5c1695e1096f5675095eaddb20
keisyaamalia <kkaysaamalia@gmail.com>
2026-05-04 13:07:28 +0700

    menjalankan program utama

--- constructorberparameter.cpp
+++ constructorberparameter.cpp
@@ -13,3 +13,9 @@ mahasiswa::mahasiswa(int nim, string nama)
     cout << "NIIM : " << nim << endl;
     cout << "Nama : " << nama << endl;
 }
+
+int main()
+{
+    mahasiswa mhs(12345, "Pascal");
+    return 0;
+}

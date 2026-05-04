24a60ae5c2042d1614e79266916f61624cefc305
keisyaamalia <kkaysaamalia@gmail.com>
2026-05-04 13:20:53 +0700

    add class buku

--- this.cpp
+++ this.cpp
@@ -1,2 +1,14 @@
 #include <iostream>
 using namespace std;
+
+class buku
+{
+    string judul;
+
+    public:
+    string setget(string judul)
+    {
+        this->judul= judul; //nilai parameter
+        return this->judul; // return variabel judul
+    }
+};
